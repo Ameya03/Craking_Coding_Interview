82aefe0379d785d678ba8035988f103124672a7c
Ameya Deshmukh <ameya03deshmukh@gmail.com>
2017-08-14 13:53:18 -0500

    1st solution

---
+++ Craking_Coding_Interview.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package craking_coding_interview;
+
+/**
+ *
+ * @author Ameya
+ */
+public class Craking_Coding_Interview {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
